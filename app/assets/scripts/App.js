52e1c519f569b831f80bd0868bba8f26d8be0b49
Ron <highratefilms@gmail.com>
2019-04-22 18:52:20 -0400

    Completed Sticky Header Functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".rotate"), "90%");
@@ -8,3 +9,5 @@ new RevealOnScroll($(".headline"), "80%");
 new RevealOnScroll($(".headline--body"), "70%");
 new RevealOnScroll($(".performance--header"), "85%");
 new RevealOnScroll($(".performance-item"), "60%");
+
+var stickyHeader = new StickyHeader();
