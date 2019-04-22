7af58e7c8504cdedd59c38585bf5110e906f4701
Ron <highratefilms@gmail.com>
2019-04-22 17:29:30 -0400

    Completed the reveal on scroll behaviour.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".rotate"), "90%");
+new RevealOnScroll($(".headline"), "80%");
+new RevealOnScroll($(".headline--body"), "70%");
+new RevealOnScroll($(".performance--header"), "85%");
+new RevealOnScroll($(".performance-item"), "60%");
