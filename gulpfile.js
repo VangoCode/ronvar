26619c2327f7febd07c358f5019dfda317ce9505
Ron <highratefilms@gmail.com>
2019-04-26 16:19:32 -0400

    Completed the gulp build task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,210 @@
-require('./gulp/tasks/styles');
+/*require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 
 require('./gulp/tasks/scripts');
+
+require('./gulp/tasks/build');*/
+
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba'),
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename'),
+del = require('del'),
+svg2png = require('gulp-svg2png'),
+webpack = require('webpack'),
+modernizr = require('gulp-modernizr'),
+imagemin = require('gulp-imagemin'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+gulp.task('styles', function() {
+    console.log("Began styles function");
+    
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .on('error', function(errorInfo) {
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+    
+    browserSync.init({
+        notify: true,
+        server: {
+            baseDir: "app"
+        }
+    });
+    
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+    
+    watch('./app/assets/styles/**/*.css', gulp.series('styles'), function() {
+        cssInject();
+    });
+    
+    watch('./app/assets/scripts/**/*.js', gulp.series('modernizr', 'scripts'));
+    
+});
+
+
+function cssInject() {
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+}
+
+var config = {
+    shape: {
+        spacing: {
+            padding: 1
+        }
+    },
+    mode: {
+        css: {
+            variables: {
+                replaceSvgWithPng: function() {
+                    return function(sprite, render) {
+                        return render(sprite).split('.svg').join('.png');
+                    }
+                }
+            },
+            sprite: 'sprite.svg',
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }
+        }
+    }
+}
+
+gulp.task('beginClean', function() {
+    return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', function() {
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('createPngCopy', function() {
+    return gulp.src('./app/temp/sprite/css/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest('./app/temp/sprite/css'));
+});
+
+gulp.task('copySpriteGraphic', function() {
+    return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', function() {
+    return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task ('endClean', function() {
+    return del('./app/temp/sprite');
+});
+
+gulp.task('icons', gulp.series('beginClean', 'createSprite', 'createPngCopy', 'copySpriteGraphic', 'copySpriteCSS', 'endClean'));
+
+
+
+
+
+
+gulp.task('scripts', function(callback) {
+    webpack(require('./webpack.config.js'), function(err, stats) {
+        if (err) {
+            console.log(err.toString());
+        }
+        console.log(stats.toString());
+        scriptsRefresh();
+        callback();
+    });
+});
+
+function scriptsRefresh() {
+    browserSync.reload();
+}
+
+
+
+
+gulp.task('modernizr', function() {
+    return gulp.src(['./app/assets/styles/**/*.css', './app/assets/scripts/**/*.js'])
+    .pipe(modernizr({
+        "options": [
+            "setClasses"
+        ]
+    }))
+    .pipe(gulp.dest('./app/temp/scripts/'));
+});
+
+
+/* DISTRIBUTION */
+
+gulp.task('previewDist', function() {
+    browserSync.init({
+        notify: true,
+        server: {
+            baseDir: "dist"
+        }
+    });
+});
+
+gulp.task('deleteDistFolder', function() {
+    return del("./dist");
+});
+
+gulp.task('copyGeneralFiles', function() {
+    var pathsToCopy = [
+        './app/**/*',
+        '!./app/index.html',
+        '!./app/assets/images/**',
+        '!./app/assets/styles/**',
+        '!./app/assets/scripts/**',
+        '!./app/temp',
+        '!./app/temp/**'
+    ]
+    
+    return gulp.src(pathsToCopy)
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('optimizeImages', function() {
+    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+    .pipe(imagemin({
+        progressive: true,
+        interlaced: true,
+        multipass: true
+    }))
+    .pipe(gulp.dest("./dist/assets/images"));
+});
+
+gulp.task('usemin', function() {
+    return gulp.src("./app/index.html")
+    .pipe(usemin({
+        css: [function() {return rev()}, function() {return cssnano()}],
+        js: [function() {return rev()}, function() {return uglify()}]
+    }))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('build', gulp.series('deleteDistFolder', 'beginClean', 'modernizr', /*'createSprite',*/ 'createPngCopy', 'copySpriteCSS', 'scripts', 'copySpriteGraphic', 'endClean', 'styles', 'copyGeneralFiles', 'optimizeImages', 'usemin'));
